66458d2c5a61282f01fff5b0b566a5408583ac90
Pimentelta <46574244+Pimentelta@users.noreply.github.com>
2019-10-17 13:02:12 -0400

    Powerpoint Updated

--- src/com/csci360/activitytracker/view/activityTrackerGUI.java
+++ src/com/csci360/activitytracker/view/activityTrackerGUI.java
@@ -3,7 +3,7 @@ package com.csci360.activitytracker.view;
 //Imports are listed in full to show what's being used
 //could just import javafx.*
 import javafx.application.Application;
-
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -15,6 +15,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
@@ -22,6 +23,14 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.application.Application;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import com.csci360.activitytracker.model.ActivityGraph;
 
@@ -76,17 +85,24 @@ public class activityTrackerGUI extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 //The primaryStage is the top-level container
-	
-	    Parent root = FXMLLoader.load(getClass().getResource("DeleteModify.fxml"));
-	    Scene scene = new Scene(root);
+		URL url = getClass().getResource("NewSession.fxml");
+		if (url == null) {
+		    System.out.println("No FXML file found, \"" + "NewSession.fxml" + "\"");
+		    Platform.exit();
+		    return;
+		}
+		FXMLLoader loader = new FXMLLoader(url);
+		AnchorPane root = loader.<AnchorPane>load();
+	    Parent root1 = FXMLLoader.load(getClass().getResource("NewSession.fxml"));
+	    Scene scene = new Scene(root1, 400, 400);
 	    
 	    String css = activityTrackerGUI.class.getResource("application.css").toExternalForm();
 	    //Parent root1 = (Parent) root.load();
 	    scene.getStylesheets().add(css);
 	    stage.setTitle("StatsWrap");
 	   // Stage stage = new Stage();
-	    stage.initModality(Modality.APPLICATION_MODAL);
-	    stage.initStyle(StageStyle.UNDECORATED);
+	   // stage.initModality(Modality.APPLICATION_MODAL);
+	   // stage.initStyle(StageStyle.UNDECORATED);
 	    //stage.setTitle("StatsWrap");
 	    stage.setScene(scene);  
 	    stage.show();
@@ -100,28 +116,7 @@ componentLayout.setPadding(new Insets(20,0,20,20));
 //The FlowPane is a container that uses a flow layout
 final FlowPane choicePane = new FlowPane();
 choicePane.setHgap(100);
-Label choiceLbl = new Label("Fruits");
-//The choicebox is populated from an observableArrayList
-ChoiceBox fruits = new ChoiceBox(FXCollections.observableArrayList("Asparagus", "Beans", "Broccoli", "Cabbage"
-, "Carrot", "Celery", "Cucumber", "Leek", "Mushroom"
-, "Pepper", "Radish", "Shallot", "Spinach", "Swede"
-, "Turnip"));
-//Add the label and choicebox to the flowpane
-choicePane.getChildren().add(choiceLbl);
-choicePane.getChildren().add(fruits); 
-//put the flowpane in the top area of the BorderPane
-componentLayout.setTop(choicePane);
-final FlowPane listPane = new FlowPane();
-listPane.setHgap(100);
-Label listLbl = new Label("Vegetables");
-ListView vegetables = new ListView(FXCollections.observableArrayList("Apple", "Apricot", "Banana"
-,"Cherry", "Date", "Kiwi", "Orange", "Pear", "Strawberry"));
-listPane.getChildren().add(listLbl);
-listPane.getChildren().add(vegetables);
-listPane.setVisible(false);
-componentLayout.setCenter(listPane);
-//The button uses an inner class to handle the button click event
-Button vegFruitBut = new Button("Fruit or Veg");
+
 vegFruitBut.setOnAction(new EventHandler() {
 @Override
 public void handle(ActionEvent event) {
