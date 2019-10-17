19851a51e85bde818eb24c8dd261fc61052ce9fd
Pimentelta <46574244+Pimentelta@users.noreply.github.com>
2019-10-17 09:05:08 -0400

    Controllers Added
    Controller for FXML files added

---
+++ src/com/csci360/activitytracker/view/HomeButtonController.java
@@ -0,0 +1,20 @@
+package com.csci360.activitytracker.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+//comes from DeleteModify fxml
+public class HomeButtonController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
---
+++ src/com/csci360/activitytracker/view/NewGoalController.java
@@ -0,0 +1,21 @@
+package com.csci360.activitytracker.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+
+public class NewGoalController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
+
